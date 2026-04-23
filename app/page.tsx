2e5036e91bdf6591c218e27bc84945b8e9aa2dd0
muflihwjd <muflih.wjd@gmail.com>
2026-04-23 12:18:18 +0800

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Hello Bang Maxim
           </a>
         </div>
       </main>
