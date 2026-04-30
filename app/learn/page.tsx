c283f465dab88be7e192e28e258967e14e805776
muflihwjd <muflih.wjd@gmail.com>
2026-04-30 12:31:21 +0800

    lanjut

---
+++ app/learn/page.tsx
@@ -0,0 +1,15 @@
+export default function Learnpage() {
+    return (
+         <div className="bg-purple-300">
+              <div className="bg-orange-300">
+                 <img/>
+                <p>Design </p>
+             </div>
+             <div>
+                <h1> FIGMA </h1>
+               <p>8 hours ago</p>
+             </div>
+         </div>
+    )
+}
+
