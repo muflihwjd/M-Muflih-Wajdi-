71945f7adef4c2be6fcca2282e8bbf9da61bbb91
muflihwjd <muflih.wjd@gmail.com>
2026-04-30 15:13:04 +0800

    laksanakan

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,15 +1,39 @@
 export default function Learnpage() {
     return (
-         <div className="bg-purple-300">
-              <div className="bg-orange-300">
-                 <img/>
+         <div className="bg-white h-screen flex gap-6 justify-center">
+         <div className="bg-purple-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+              <div>
+                <img src="/palette.svg"/>
                 <p>Design </p>
              </div>
              <div>
-                <h1> FIGMA </h1>
-               <p>8 hours ago</p>
+                <h1 className="text-4xl font-bold">FIGMA </h1>
+               <p className="text-lg"> 8 hours ago</p>
              </div>
          </div>
+
+         <div className="bg-purple-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+              <div>
+                <img src="/palette.svg"/>
+                <p>Design </p>
+             </div>
+             <div>
+                <h1 className="text-4xl font-bold">FIGMA </h1>
+               <p className="text-lg"> 8 hours ago</p>
+             </div>
+         </div>
+
+         <div className="bg-purple-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+              <div>
+                <img src="/palette.svg"/>
+                <p>Design </p>
+             </div>
+             <div>
+                <h1 className="text-4xl font-bold">FIGMA </h1>
+               <p className="text-lg"> 8 hours ago</p>
+             </div>
+         </div>
+         </div>
+         
     )
 }
-
